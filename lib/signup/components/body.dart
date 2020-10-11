c3546e5c4e0db1d7f5754fb5c05bf934aa504594
Sai Kumar <adepusai123@gmail.com>
2020-10-11 09:44:02 +0530

    background component created

---
+++ lib/signup/components/body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:login_signup/signup/components/background.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Background(
+        child: Column(
+      children: <Widget>[],
+    ));
+  }
+}
--- lib/signup/signup_screen.dart
+++ lib/signup/signup_screen.dart
@@ -1,47 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:login_signup/signup/components/body.dart';
 
 class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Body(
-      child: Column(),
-    ));
-  }
-}
-
-class Body extends StatelessWidget {
-  final Widget child;
-  const Body({
-    Key key,
-    @required this.child,
-  }) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center,
-        children: <Widget>[
-          Positioned(
-              top: 0,
-              left: 0,
-              child: Image.asset(
-                "assets/images/signup_top.png",
-                width: size.height * 0.25,
-              )),
-          Positioned(
-              bottom: 0,
-              left: 0,
-              child: Image.asset(
-                "assets/images/main_bottom.png",
-                width: size.height * 0.15,
-              )),
-          child
-        ],
-      ),
-    );
+    return Scaffold(body: Body());
   }
 }
